feef0da0931f9e4d2bc1c41a80fd379e0d8fdecc
Kim Ji Seob <jiseob812@gmail.com>
2019-07-29 17:43:41 +0900

    앱 기초 나누기 완료 - 아직 고급편 안들어감

--- Practice_Config/ViewController.swift
+++ Practice_Config/ViewController.swift
@@ -13,8 +13,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let label = UILabel(frame: .zero)
+        label.text = "서버는 블라블라"
+        label.sizeToFit()
+        label.center = self.view.center
+        self.view.addSubview(label)
+        
+        if let url = Bundle.main.object(forInfoDictionaryKey: "Server URL") as? String {
+            print("plist에서 받은 값은 \(url)")
+        }
     }
-
-
 }
 
