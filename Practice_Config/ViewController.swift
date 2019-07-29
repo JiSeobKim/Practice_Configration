178ce5320178a1944673f7987d9219fd300baaf4
Kim Ji Seob <jiseob812@gmail.com>
2019-07-29 10:09:04 +0900

    Initial Commit

---
+++ Practice_Config/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practice_Config
+//
+//  Created by kimjiseob on 29/07/2019.
+//  Copyright © 2019 kimjiseob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
